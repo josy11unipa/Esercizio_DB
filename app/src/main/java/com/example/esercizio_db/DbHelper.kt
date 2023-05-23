a7f917de63f55c97205251f877a33e0746bb2029
guido <guido.lamia@community.unipa.it>
2023-05-23 15:49:22 +0200

    sw

--- app/src/main/java/com/example/esercizio_db/DbHelper.kt
+++ app/src/main/java/com/example/esercizio_db/DbHelper.kt
@@ -1,6 +1,33 @@
 package com.example.esercizio_db
 
-class DbHelper {
-    //onCreate()
-    //onUpgrade()
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DbHelper(context:Context):SQLiteOpenHelper(context,DaTABASE_NAME,null,DATABASE_VERSION) {
+
+companion object{
+    const val DaTABASE_NAME="Materie.db"
+    const val DATABASE_VERSION=1
+    const val TABLE_NAME="materia"
+    const val ID="id"
+    const val SUBJECT="subject"
+    const val DESC="description"
+    private const val DB_CREATE=
+        "CREATE TABLE" +
+            " ${TABLE_NAME} (${ID} INTEGER PRIMARY KEY,"+
+            " ${SUBJECT} TEXT NOT NULL," +
+                "${DESC} TEXT );"
+    const val SQL_DELETE="DROP TABLE "+
+            "IF EXISTS ${TABLE_NAME};"
+}
+
+    override fun onCreate(db: SQLiteDatabase?) {
+       db?.execSQL(DB_CREATE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(SQL_DELETE)
+        onCreate(db)
+    }
 }
