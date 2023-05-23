93ae93e8f46e0f9ce244d3c76d7b3ef989eb75d7
guido <guido.lamia@community.unipa.it>
2023-05-23 18:12:39 +0200

    sw

--- app/src/main/java/com/example/esercizio_db/Activity2.kt
+++ app/src/main/java/com/example/esercizio_db/Activity2.kt
@@ -1,5 +1,7 @@
 package com.example.esercizio_db
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.esercizio_db.databinding.ActivityAddBinding
@@ -11,7 +13,24 @@ class Activity2:AppCompatActivity (){
         binding=ActivityAddBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        dbManager=DBManager(this)
+        dbManager= DBManager(this)
         dbManager.open()
+        binding.confirmNewAdd.setOnClickListener{
+            val subject=binding.nameNewAdd.text.toString()
+            val desc=binding.descNewAdd.text.toString()
+            dbManager.insert(subject,desc)
+            val intent=Intent(this,MainActivity::class.java)
+            intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TOP
+            setResult(Activity.RESULT_OK,intent)
+            startActivity(intent)
+
+
+        }
+
     }
+     fun OnDestroy(){
+        dbManager.close()
+        super.onDestroy()
+    }
+
 }
--- app/src/main/java/com/example/esercizio_db/MainActivity.kt
+++ app/src/main/java/com/example/esercizio_db/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.esercizio_db
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
@@ -15,26 +18,42 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var dbManager: DBManager
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        dbManager=DBManager(this)
+        dbManager = DBManager(this)
         dbManager.open()
-        val cursor=dbManager.fetchAll()
-        if(cursor.count!=0){
-            binding.recycler.layoutManager=LinearLayoutManager(this)
-            binding.recycler.adapter=CustomAdapter(cursor)
-        }else{
-            Toast.makeText(this,"il database è vuoto",Toast.LENGTH_SHORT).show()
+        val cursor = dbManager.fetchAll()
+        if (cursor.count != 0) {
+            binding.recycler.layoutManager = LinearLayoutManager(this)
+            binding.recycler.adapter = CustomAdapter(cursor)
+        } else {
+            Toast.makeText(this, "il database è vuoto", Toast.LENGTH_SHORT).show()
         }
 
 
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; questo aggiunge elementi alla barra delle azioni se è presente.
-        val inflater:MenuInflater=menuInflater
+        val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.addB -> {
+                val intent = Intent(this, Activity2::class.java)
+                setResult(Activity.RESULT_OK, intent)
+                startActivity(intent)
+
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
