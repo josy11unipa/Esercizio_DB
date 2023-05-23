570cc67a1443ef382232c626f81b44bbf5243567
Joseph <joseph.mangiapane@community.unipa.it>
2023-05-23 11:57:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/esercizio_db/DBManager.kt
@@ -0,0 +1,17 @@
+package com.example.esercizio_db
+
+class DBManager {
+
+    public fun open(){}
+
+    public fun close(){}
+
+    public fun fetchall(){}
+
+    public fun delete(){}
+
+    public fun update(){}
+
+    public fun insert(){}
+
+}
---
+++ app/src/main/java/com/example/esercizio_db/DbHelper.kt
@@ -0,0 +1,6 @@
+package com.example.esercizio_db
+
+class DbHelper {
+    //onCreate()
+    //onUpgrade()
+}
