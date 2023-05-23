ff89fefca15aa41f33972dd7c159e69ba90d9a90
guido <guido.lamia@community.unipa.it>
2023-05-23 17:31:38 +0200

    sw

--- app/src/main/java/com/example/esercizio_db/CustomAdapter.kt
+++ app/src/main/java/com/example/esercizio_db/CustomAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.esercizio_db
 
+import android.annotation.SuppressLint
+import android.database.Cursor
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.esercizio_db.databinding.CardViewBinding
 
-class CustomAdapter(private val data:Array<String>) :RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter(private val cursor:Cursor) :RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
     class ViewHolder(binding:CardViewBinding):RecyclerView.ViewHolder(binding.root) {
         val textID=binding.id
         val textName=binding.name
@@ -15,15 +18,22 @@ class CustomAdapter(private val data:Array<String>) :RecyclerView.Adapter<Custom
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view=CardViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+      return cursor.count
     }
 
+    @SuppressLint("Range")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if(cursor.moveToPosition(position)){
+            holder.textID.text=cursor.getLong(cursor.getColumnIndex(DbHelper.ID)).toString()
+            holder.textName.text=cursor.getString(cursor.getColumnIndex(DbHelper.SUBJECT))
+            holder.textDesc.text=cursor.getString(cursor.getColumnIndex(DbHelper.DESC))
+
+        }
     }
 
 
--- app/src/main/java/com/example/esercizio_db/MainActivity.kt
+++ app/src/main/java/com/example/esercizio_db/MainActivity.kt
@@ -2,13 +2,34 @@ package com.example.esercizio_db
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.example.esercizio_db.databinding.ActivityMainBinding
+import com.example.esercizio_db.databinding.CardViewBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var dbManager: DBManager
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        dbManager=DBManager(this)
+        dbManager.open()
+        val cursor=dbManager.fetchAll()
+        if(cursor.count!=0){
+            binding.recycler.layoutManager=LinearLayoutManager(this)
+            binding.recycler.adapter=CustomAdapter(cursor)
+        }else{
+            Toast.makeText(this,"il database è vuoto",Toast.LENGTH_SHORT).show()
+        }
+
+
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; questo aggiunge elementi alla barra delle azioni se è presente.
