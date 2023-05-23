55ad765b5fcd959c696401f619429c15f9c60285
Joseph <joseph.mangiapane@community.unipa.it>
2023-05-23 17:10:52 +0200

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/esercizio_db/DBManager.kt
+++ app/src/main/java/com/example/esercizio_db/DBManager.kt
@@ -1,17 +1,55 @@
 package com.example.esercizio_db
 
-class DBManager {
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 
-    public fun open(){}
+class DBManager(val context: Context) {
+    private lateinit var db_helper: DbHelper
+    private lateinit var db: SQLiteDatabase
 
-    public fun close(){}
+    fun open() : DBManager{
+          db_helper = DbHelper(context)
+        db = db_helper.writableDatabase
+        return this
+    }
 
-    public fun fetchall(){}
+    fun close(){
+        db_helper.close()
+    }
 
-    public fun delete(){}
+    fun insert(name: String, desc:String){
+        val value = ContentValues().apply{
+            put(DbHelper.SUBJECT,name)
+            put(DbHelper.DESC,desc)
+        }
+        db.insert(DbHelper.TABLE_NAME, null, value)
+    }
 
-    public fun update(){}
+    fun update(id:Long, name: String, desc: String) : Int{
+        val selection = "${DbHelper.ID} = ? "
+        val selectionArgs = arrayOf(id.toString())
 
-    public fun insert(){}
+        val values = ContentValues().apply{
+            put(DbHelper.SUBJECT,name)
+            put(DbHelper.DESC,desc)
+        }
+        val n = db.update(DbHelper.TABLE_NAME, values, selection, selectionArgs)
+        return n
+    }
+    fun delete(id: Long){
+        val selection = "${DbHelper.ID} = ? "
+        val selectionArgs = arrayOf(id.toString())
+        db.delete(DbHelper.TABLE_NAME, selection, selectionArgs)
+    }
 
+    fun fetchAll(): Cursor{
+        val projection = arrayOf(DbHelper.ID, DbHelper.SUBJECT, DbHelper.DESC)
+        val cursor = db.query(
+            DbHelper.TABLE_NAME, projection, null, null, null, null, null
+        )
+        cursor?.moveToFirst()
+        return cursor
+    }
 }
